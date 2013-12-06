ee55d6885674a71efded0c616e28a54cdfe072fb
Arnold Sikorski <asikorski@sointeractive.pl>
2013-12-06 11:33:56 +0100

    add examle files

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,12 +8,13 @@ object Main {
   def main(args: Array[String]):Unit = {
     var buf: Option[BufferedSource] = None
     
-    buf = Some(Source.fromFile("data/simple.raml"))
+    buf = Some(Source.fromFile("data/example.raml"))
     val source: String = buf.get.getLines mkString "\n"
     
     var raml:Raml = new RamlDocumentBuilder().build(source);
     
     var version = raml.getVersion()
+    println(raml)
     println(s"test $version")
   } 
 }

a82d6368df51cbc00524066e4e7d235e20114f80
Arnold Sikorski <asikorski@sointeractive.pl>
2013-12-11 12:23:24 +0100

    add configuration file, clean up main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,43 +2,53 @@ import scala.io.BufferedSource
 import scala.io.Source
 import org.raml.model.Raml
 import org.raml.parser.visitor.RamlDocumentBuilder
-
 import analyser.Package
-
 import generator.Context
 import generator.CodeContext
 import generator.Generator
 import generator.PhpSDKGenerator
 
+import com.typesafe.config._
+
 object Main {
 
-	def main(args: Array[String]): Unit = {
-		/**@TODO add command line user interace*/	
-		var buf: Option[BufferedSource] = None
+  val config = ConfigFactory.load()
 
-		buf = Some(Source.fromFile("data/example.raml"))
-		val source: String = buf.get.getLines mkString "\n"
+  def main(args: Array[String]): Unit = {
 
+    /**from command line -g --generator, -o --output, -r --raml */
+    val ramlPath = "data/example.raml"
+    val outputDirectory = "./generated"
+      
+    /** Get configuration for application*/
+    var application = config.getConfig("application")
 
-		/**from configuration*/
-		val baseUrl = "https://api.isaacloud/v1/api"
-		val resourcePath = "./resources/php"
-		val tempDirectory = "/tmp/gen_tmp/"
+    /**@TODO add command line user interace*/
+    
+    /**Load RAML file*/
+    var buf: Option[BufferedSource] = None
+    buf = Some(Source.fromFile("data/example.raml"))
+    val source: String = buf.get.getLines mkString "\n"
 
-		/**from command line -g --generator, -o --output*/
-		val outputDirectory = "./generated"
+    /** Get data from configuration*/
+    val baseUrl = application.getString("baseUrl")
+    val resourcePath = application.getString("resourcePath")
+    val tempDirectory = application.getString("tempDirectory")
 
-		val codeGenerator: Generator = new PhpSDKGenerator
-		var raml: Raml = new RamlDocumentBuilder().build(source)
+    
 
-		var composer: CodeContext = new CodeContext()
-		composer.withBaseUrl(baseUrl)
-			.withGenerator(codeGenerator)
-			.withOutputDirectory(outputDirectory)
-			.withRaml(raml)
-			.withResourcePath(resourcePath)
-			.withTempDirectory(tempDirectory)
-			.compose
+    val codeGenerator: Generator = new PhpSDKGenerator
+    var raml: Raml = new RamlDocumentBuilder().build(source)
 
-	}
+    /**Invoke code generator*/
+    var composer: CodeContext = new CodeContext()
+    composer.withBaseUrl(baseUrl)
+      .withGenerator(codeGenerator)
+      .withOutputDirectory(outputDirectory)
+      .withRaml(raml)
+      .withResourcePath(resourcePath)
+      .withTempDirectory(tempDirectory)
+      .compose
+
+  }
 }

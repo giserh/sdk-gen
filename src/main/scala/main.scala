234db4b79996c95eb5849f2cc74dc7eba3234594
Arnold Sikorski <asikorski@sointeractive.pl>
2013-12-11 17:24:38 +0100

    add option mechanism

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -19,54 +19,55 @@ object Main {
   val config = ConfigFactory.load()
 
   /**Get runner option*/
-  type OptionMap = Map[Symbol, Any]
   val usage = """ Usage: sdkgen [--generator | -g string] [--output -o directory] filename """
-  def nextOption(map: OptionMap, list: List[String]): OptionMap = {
+  def nextOption(map: Map[String,_], list: List[String]): Map[String,_] = {
     list match {
       case Nil => map
       case ("--generator" | "-g") :: value :: tail => value match {
         /** @TODO add new generators */
-        case "php" => nextOption(map ++ Map('generator -> new PhpSDKGenerator), tail)
-        case _ => throw new CodeWorkerException(s"Not found generator: $value")
+        case "php" => nextOption(map ++ Map("generator" -> new PhpSDKGenerator), tail)
+        case _ => throw new InvalidParameterException(s"Not found generator: $value")
       }
       case ("--output" | "-o") :: value :: tail => {
         /** @TODO validate output*/
-        nextOption(map ++ Map('output -> value.toString), tail)
+        nextOption(map ++ Map("output" -> value.toString), tail)
       }
       /**get last parameter - path to RAML file */
       case string :: Nil => {
         /** @TODO check file is exist , in future get file from remote resource*/
-        nextOption(map ++ Map('raml -> string), list.tail)
+        nextOption(map ++ Map("raml" -> string), list.tail)
       }
       case option :: tail =>
         println("Unknown option " + option)
         exit(1)
     }
   }
+  
   def main(args: Array[String]): Unit = {
 
     /**Check whether the parameters passed*/
     if (args.length != 0) {
 
+      
       /**catch options*/
-      val options: OptionMap = nextOption(Map(), args.toList)
+      val options: Map[String,Any] = nextOption(Map(), args.toList)
       
-      if(!options.isDefinedAt('generator)) throw new InvalidParameterException("Generator are not definded!")
-      if(!options.isDefinedAt('output)) throw new InvalidParameterException("Output directory are not definded!")
-      if(!options.isDefinedAt('raml)) throw new InvalidParameterException("RAML file are not definded!")
+
+      if(!options.contains("generator")) throw new InvalidParameterException("Generator are not definded!")
+      if(!options.contains("output")) throw new InvalidParameterException("Output directory are not definded!")
+      if(!options.contains("raml")) throw new InvalidParameterException("RAML file are not definded!")
       
-      
-      /**from command line -g --generator, -o --output, -r --raml */
-      val ramlFile = "data/example.raml"
-      val outputDirectory = "./generated"
-      val codeGenerator: Generator = new PhpSDKGenerator
+      /**from command line -g --generator, -o --output raml */
+      val ramlFile = options.get("raml").get
+      val outputDirectory = options.get("output").get
+      val codeGenerator = options.get("generator").get
       
       /** Get configuration for application*/
       var application = config.getConfig("application")
 
       /**Load RAML file*/
       var buf: Option[BufferedSource] = None
-      buf = Some(Source.fromFile(ramlFile))
+      buf = Some(Source.fromFile(ramlFile.asInstanceOf[String]))
       val source: String = buf.get.getLines mkString "\n"
 
       /** Get data from configuration*/
@@ -80,8 +81,8 @@ object Main {
       /**Invoke code generator*/
       var composer: CodeContext = new CodeContext()
       composer.withBaseUrl(baseUrl)
-        .withGenerator(codeGenerator)
-        .withOutputDirectory(outputDirectory)
+        .withGenerator(codeGenerator.asInstanceOf[Generator])
+        .withOutputDirectory(outputDirectory.toString)
         .withRaml(raml)
         .withResourcePath(resourcePath)
         .withTempDirectory(tempDirectory)

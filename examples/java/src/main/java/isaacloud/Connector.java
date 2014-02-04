af4f736584bf3403a2f779eef4818378e13b1cdf
tgodzik <tomek.godzik@gmail.com>
2014-02-04 10:33:24 +0100

    Added logging and a script.

--- examples/java/src/main/java/isaacloud/Connector.java
+++ examples/java/src/main/java/isaacloud/Connector.java
@@ -31,12 +31,15 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+import java.util.logging.Logger;
 
 public class Connector {
 
 	private String baseUrl;
 	private String version;
 
+	private final static Logger logger = Logger.getLogger(Connector.class
+			.getName());
 	private static String oauthUrl;
 	private static String clientId;
 	private static String clientSecret;
@@ -125,13 +128,26 @@ public class Connector {
 
 				EntityUtils.consume(entity1);
 				response.close();
+				
+				int status = response.getStatusLine().getStatusCode();
+				
+				if (status != 200 ){
+					if (result.toString().trim().length() == 0)
+						logger.severe("Call was unsuccessful, code returned:\n" + response.getStatusLine());
+					else
+						logger.severe("Call was unsuccessful, code returned:\n" + response.getStatusLine() + "\nReason: \n" + result);
+					return "Bearer " + currentToken;
+				}				
+				
+				JSONObject obj = new JSONObject(result.toString());
+
+				Connector.currentToken = obj.get("access_token").toString();
+				
 			} catch (Exception e) {
-				// log
+				logger.severe("Error retrieving access token, reason:\n"+e.getMessage());
 			}
 
-			JSONObject obj = new JSONObject(result.toString());
-
-			Connector.currentToken = obj.get("access_token").toString();
+			
 		}
 		return "Bearer " + currentToken;
 	}
@@ -151,19 +167,34 @@ public class Connector {
 			CloseableHttpResponse response = client.execute(method);
 
 			HttpEntity entity1 = response.getEntity();
-
+			
+			int status = response.getStatusLine().getStatusCode();
+					
+			
+			
 			BufferedReader rd = new BufferedReader(new InputStreamReader(
 					entity1.getContent()));
 
 			String line = "";
+			
 			while ((line = rd.readLine()) != null) {
 				result.append(line);
 			}
 
 			EntityUtils.consume(entity1);
+			
 			response.close();
+			
+			if (status != 200 && status !=201){
+				if (result.toString().trim().length() == 0)
+					logger.warning("Call was unsuccessful, code returned:\n" + response.getStatusLine());
+				else
+					logger.warning("Call was unsuccessful, code returned:\n" + response.getStatusLine() + "\nReason: \n" + result);
+				return "{}";
+			}
+			
 		} catch (Exception e) {
-			// log
+			logger.severe("Could not retrieve result, reason:\n"+e.getMessage());
 		}
 
 		return result.toString();
---
+++ script/fixit.py
@@ -0,0 +1,90 @@
+import json
+import collections
+
+def create_get(filename, newfilename):
+    print filename
+    file1 = open(filename, "r")
+
+    js = json.loads(file1.read())
+
+    file1.close()
+
+    if "id" not in js:
+        js["id"] = 1
+
+    od = collections.OrderedDict(sorted(js.items()))
+
+    if "updatedAt" in od:
+        del od["updatedAt"]
+
+    od["updatedAt"] = 1390236382695
+
+    if "createdAt" in od:
+        del od["createdAt"]
+
+    od["createdAt"] = 1390236382695
+    ret = json.dumps(od, indent=4, separators=(',', ': '))
+
+    file1 = open(newfilename, "w")
+    file1.write(ret)
+
+
+def create_post(filename,newfilename):
+    file1 = open(filename, "r")
+
+    js = json.loads(file1.read())
+
+    file1.close()
+
+    if "id" in js:
+        del js["id"]
+    if "updatedAt" in js:
+        del js["updatedAt"]
+    if "createdAt" in js:
+        del js["createdAt"]
+
+    od = collections.OrderedDict(sorted(js.items()))
+
+    ret = json.dumps(od, indent=4, separators=(',', ': '))
+    name = filename.split(".")
+    file1 = open(newfilename, "w")  # name[0] + "_post." + name[1], "w")
+    file1.write(ret)
+
+
+def create_list(filename, newfilename):
+    file1 = open(filename, "r")
+
+    js = json.loads(file1.read())
+
+    file1.close()
+
+    if "id" not in js:
+        js["id"] = 1
+
+    od = collections.OrderedDict(sorted(js.items()))
+
+    if "updatedAt" in od:
+        del od["updatedAt"]
+
+    od["updatedAt"] = 1390236382695
+
+    if "createdAt" in od:
+        del od["createdAt"]
+
+    od["createdAt"] = 1390236382695
+
+    ret = json.dumps([od], indent=4, separators=(',', ': '))
+    file1 = open(newfilename, "w")
+    file1.write(ret)
+
+
+import os
+
+for dirname, dirnames, filenames in os.walk('./examples'):
+
+    for filename in filenames:
+        whole_path = os.path.join(dirname, filename)
+        create_get(whole_path, whole_path.replace("examples", "new"))
+        create_list(whole_path, whole_path.replace("examples", "new").replace("_example", "_list_example"))
+        if "cache" not in whole_path and "queue" not in whole_path :
+            create_post(whole_path, whole_path.replace("examples", "new").replace("_example", "_post_example"))
